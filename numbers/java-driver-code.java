b11e286d0017b1ca83e78f16d9e679e55aa94379
Anantkumarjha <93377127+Anantkumarjha@users.noreply.github.com>
2022-04-08 12:47:41 +0530

    Update java-driver-code.java

--- numbers/java-driver-code.java
+++ numbers/java-driver-code.java
@@ -1,5 +1,38 @@
 import java.io.*;
 import java.util.*;
+class Magic
+{
+   public static boolean isMagic(int n)
+   {
+       int sum = 0;
+     
+       while (n > 0 || sum > 9)
+       {
+           if (n == 0)
+           {
+               n = sum;
+               sum = 0;
+           }
+           sum += n % 10;
+           n /= 10;
+       }
+      
+      
+       return (sum == 1);
+   }
+   
+  
+   public static void main(String args[])
+    {
+     int n = 1234;
+     if (isMagic(n))
+        System.out.println("Magic Number");
+          
+     else
+        System.out.println("Not a magic Number");
+    }
+}
+ 
 
 public class Main {
   	// The main function reads the input in the required format.
